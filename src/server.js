f090f0d8b27491a2fe8cb964e29d583a3f1cf51a
mintanxon <mintanxon@163.com>
2023-04-17 01:46:04 +0800

    Revert "set 8 threads in parallel for vg"
    This reverts commit 910f32d0543ba6aa07704ac71d302d9c731501bb.

--- src/server.js
+++ src/server.js
@@ -341,16 +341,14 @@ function getChunkedData(req, res, next) {
       '-b',
       `${req.chunkDir}/chunk`,
       '-E',
-      `${req.chunkDir}/regions.tsv`,
-      '--threads',
-      8
+      `${req.chunkDir}/regions.tsv`
     );
 
     console.log(`vg ${vgChunkParams.join(' ')}`);
 
     console.time('vg chunk');
     const vgChunkCall = spawn(`${VG_PATH}vg`, vgChunkParams);
-    const vgViewCall = spawn(`${VG_PATH}vg`, ['view', '-j', '-', '--threads', 8]);
+    const vgViewCall = spawn(`${VG_PATH}vg`, ['view', '-j', '-']);
     let graphAsString = '';
     req.error = new Buffer(0);
 
